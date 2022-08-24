3841ccc9203df788cfe41f488538b483cadf1316
Abdul Ali Noori <ab.noori.1993@gmail.com>
2022-08-24 23:21:45 +0430

    cart id and currency units changed.

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ let generateShop = () =>{
           <h3>${name}</h3>
           <p>${desc}</p>
             <div class="price-quantity">
-              <h2>$ ${price}</h2>
+              <h2>تومان ${price}</h2>
               <div class="buttons">
                 <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
                 <div id=${id} class="quantity">
